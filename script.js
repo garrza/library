e342570db7712d1eeef3a021bb64638afbc28e89
Ramiro <GARZA.RAMIROV@GMAIL.COM>
2023-06-09 15:06:56 -0600

    C O L O R S

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, color) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.color = color;
 }
 
 
@@ -18,15 +19,12 @@ function createBook() {
     let author = prompt("Author: ");
     let pages = prompt("Pages: ");
     let read = prompt("Read (y/n): ");
-    let book = new Book(title, author, pages, true ? read == "y" : read == "n");
+    let color = prompt("Choose a color, any color: ");
+    let book = new Book(title, author, pages, true ? read == "y" : read == "n", color);
     myLibrary.push(book);
     content.innerHTML = "";
     displayBooks();
 }
-
-
-addButton.onclick = createBook;
-
 
 function displayBooks() {
     for (let i = 0; i < myLibrary.length; i++) {
@@ -37,16 +35,22 @@ function displayBooks() {
 
         let actualBook = document.createElement("div");
         actualBook.classList.add("book");
+        actualBook.setAttribute("id", book.title);
+        
 
         let titleElement = document.createElement("div");
         titleElement.textContent = book.title;
         titleElement.classList.add("title");
-        bookContainer.appendChild(titleElement);
+        titleElement.style.backgroundColor = book.color;
+        
+        
 
         let authorElement = document.createElement("div");
         authorElement.textContent = book.author;
         authorElement.classList.add("author");
-
+        authorElement.style.color = book.color;
+        
+        bookContainer.appendChild(titleElement);
         actualBook.appendChild(titleElement);
         bookContainer.appendChild(actualBook);
         bookContainer.appendChild(authorElement);
@@ -54,3 +58,5 @@ function displayBooks() {
         content.appendChild(bookContainer);
     }
 }
+
+addButton.onclick = createBook;

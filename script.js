11332c4737723c0f4e89c98c5998c8d87410c401
Ramiro <GARZA.RAMIROV@GMAIL.COM>
2023-06-08 18:28:42 -0600

    Finalize script, final tweaks, object creation and manipulation, function to createBook and to displayBooks

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let myLibrary = [];
-
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -7,6 +5,52 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
-function addBook(book){
+
+let myLibrary = [];
+
+
+const addButton = document.getElementById("addButton");
+const content = document.getElementById("content");
+
+
+function createBook() {
+    let title = prompt("Book name: ");
+    let author = prompt("Author: ");
+    let pages = prompt("Pages: ");
+    let read = prompt("Read (y/n): ");
+    let book = new Book(title, author, pages, true ? read == "y" : read == "n");
     myLibrary.push(book);
+    content.innerHTML = "";
+    displayBooks();
+}
+
+
+addButton.onclick = createBook;
+
+
+function displayBooks() {
+    for (let i = 0; i < myLibrary.length; i++) {
+        let book = myLibrary[i];
+
+        let bookContainer = document.createElement("div");
+        bookContainer.classList.add("book-container");
+
+        let actualBook = document.createElement("div");
+        actualBook.classList.add("book");
+
+        let titleElement = document.createElement("div");
+        titleElement.textContent = book.title;
+        titleElement.classList.add("title");
+        bookContainer.appendChild(titleElement);
+
+        let authorElement = document.createElement("div");
+        authorElement.textContent = book.author;
+        authorElement.classList.add("author");
+
+        actualBook.appendChild(titleElement);
+        bookContainer.appendChild(actualBook);
+        bookContainer.appendChild(authorElement);
+
+        content.appendChild(bookContainer);
+    }
 }
